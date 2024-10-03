a5b5dca97e7ee64e2c3f7e7a7bd21d57a9b1f627
ThiagoMargoni <158624036+goesq@users.noreply.github.com>
2024-10-02 22:51:08 -0300

    Add

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -1,32 +1,55 @@
-const express = require('express');
-const router = express.Router();
-const books = require('../models/book');
+// Cria as rotas
+const express = require('express') // importa o express para criar o servidor
+const Book = require('../models/Book') // importa o modelo dos livros Book.js
+const router = express.Router(); // cria o elemento para rotear com base nas requisições
 
-// Adicionar Livro
-router.post('/books', (req, res) => {
-  const { title, author, year, isbn } = req.body;
-  const book = { title, author, year, isbn };
-  books.push(book);
-  res.status(201).send(book);
+// Cria rota do post
+// async é por conta que a requisição tem um certo tempo para retornar
+router.post('/',async(req,res)=>{
+const {title,author,year} = req.body; // Extrai os dados da requisição
+// tenta salvar o livro no banco de dados
+try{
+    const newBook = new Book({title, author, year});
+    await newBook.save();
+    // 201 - código de status 
+    res.status(201).json(newBook);
+}catch(error){
+    res.status(500).json({message:'Erro ao cadastrar livro'});
+}
+
 });
 
-// Remover Livro
-router.delete('/books/:isbn', (req, res) => {
-  const { isbn } = req.params;
-  books = books.filter(book => book.isbn !== isbn);
-  res.status(200).send({ message: 'Livro removido' });
+// rota Get
+// req - requisição
+// res - resposta
+router.get('/',async(req,res)=>{
+try{
+    const books = await Book.find(); // busca todos os livros com o metódo find
+    res.status(200).json(books) // retorna a lista de livros
+}catch(error){
+    res.status(500).json({message: 'Erro ao buscar os livros ',error}) // retorna o erro se houver
+}
 });
 
-// Listar Livros
-router.get('/books', (req, res) => {
-  res.status(200).send(books);
-});
+// put - modifica elemento 
+router.put('/:id',async(req,res)=>{
+    const {title,author,year} = req.body; // extrai o conteudo da requisição
+try{
+const updatedBook = await Book.findByIdAndUpdate(req.params.id,{title,author, year},{new: true});
+res.status(200).json(updatedBook);
+}catch(error){
+    res.status(500).json({message:'Erro ao atualizar livro',error}); 
+}
+})
+// delete - deleta elemento pelo id
+router.delete('/:id',async(req,res)=>{
+    try{
+        await Book.findByIdAndDelete(req.params.id);
+        res.status(200).json({message:'Livro deletado com sucesso'});
+    }catch(error){
+        res.status(500).json({message:'Erro ao deletar livro',error});
+    }
 
-// Buscar Livro por Título
-router.get('/books/search', (req, res) => {
-  const { title } = req.query;
-  const filteredBooks = books.filter(book => book.title.toLowerCase().includes(title.toLowerCase()));
-  res.status(200).send(filteredBooks);
-});
-
-module.exports = router;
+})
+// exporta o roteador para usar no server.js
+module.exports = router
